5417ef721db44bfbce0aa5c4c0ac94dd6dccc026
mastermind braveson <ebubekir.durukal@agu.edu.tr>
2018-05-08 00:40:59 +0300

    Add files via upload

---
+++ http_get_uno.ino
@@ -0,0 +1,23 @@
+int sensor=12;
+int val=0;
+char incomingByte = 0;   // for incoming serial data
+String yazi="";
+void setup() {
+  // put your setup code here, to run once:
+  Serial1.begin(115200);
+  Serial.begin(115200);
+}
+
+void loop() {
+ while (Serial1.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial1.read();
+                yazi+=incomingByte;
+                // say what you got:
+               // Serial.print("I received: ");
+        }
+        if(!yazi.equals("")) {
+                        Serial.print(yazi);
+                        }
+yazi="";
+}
